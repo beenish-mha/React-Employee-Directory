12e955ca3916f40e50414aef156ba45f122db640
beenish <beeni.khurram@gmail.com>
2020-03-22 16:57:43 +0000

    linked bootstrap and make Nav component

---
+++ react-employee-directory/src/components/Nav.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+class Nav extends React.Component {
+  constructor() {
+    super();
+  }
+  render() {
+    return (
+      <nav class="navbar navbar-light bg-dark">
+        <span class="navbar-brand mb-0 h1">Employee Directory</span>
+      </nav>
+    );
+  }
+}
+export default Nav;
