6c190222b667e498470a851d8c27a3af85d1737f
beenish <beeni.khurram@gmail.com>
2020-04-01 14:03:01 +0100

    formate date of birth

--- src/components/EmployeeTable.js
+++ src/components/EmployeeTable.js
@@ -18,6 +18,16 @@ class EmployeeTable extends React.Component {
     });
   }
 
+  formatDate(date) {
+    const dateArray = date.split("-");
+    const year = dateArray[0];
+    const month = dateArray[1];
+    const dayArray = dateArray[2].split("T");
+    const day = dayArray[0];
+    const formattedDate = [month, day, year].join("-");
+    return formattedDate;
+  }
+
   render() {
     const { name } = this.state.employees[0];
     console.log(name);
@@ -57,7 +67,7 @@ class EmployeeTable extends React.Component {
               </td>
               <td>{this.state.employees[0].phone}</td>
               <td>{this.state.employees[0].email}</td>
-              <td>{this.state.employees[0].dob.date}</td>
+              <td>{this.formatDate(this.state.employees[0].dob.date)}</td>
             </tr>
           </tbody>
         </table>
